3bb36971d004d71c9cc986d882111e580cca5e31
musickharkov <musickharkovcom@gmail.com>
2022-11-07 18:30:50 +0200

    adding gitignore

--- components/sidebar/components/mocked/mocked.tsx
+++ components/sidebar/components/mocked/mocked.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from "~/components/shared/button";
 
-const Mocked: React.FC<{onApply: () => void}> = ({ onApply }) => {
+const Mocked: React.FC<{onApply: () => void, onReset: () => void}> = ({ onApply, onReset}) => {
   return (
     <div className='w-full border-b py-2'>
       <h3 className='text-xl mb-2'>MOCKED DATA</h3>
-      <Button title='Apply mocked data' onClick={onApply}/>
+      <div className='flex flex-col gap-2'>
+        <Button title='Apply mocked data' onClick={onApply}/>
+        <Button title='Reset data' onClick={onReset}/>
+      </div>
     </div>
   );
 };
